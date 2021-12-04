33935705f9f3df3a8c66598b84284576b0e563cb
ozanulutas <oznulutas@gmail.com>
2021-12-04 23:26:50 +0300

    feat: create select all and create apis

--- db/facility.js
+++ db/facility.js
@@ -1,8 +1,49 @@
-const getAll = () => {
+const db = require("./db");
+
+// Returns all facility records
+const getAll = async () => {
   console.log("facility data access layer");
+  try {
+    let result = await db.query(`
+      SELECT custom_col.name, custom_col.alias FROM 
+      custom_col JOIN tbl 
+      ON tbl.id = custom_col.tbl_id 
+      where tbl.name = 'facility'
+    `)
+    const cols = result.rows
+
+    result = await db.query(`
+      SELECT id, name, membership_start_date, membership_end_date, employees, is_special
+      ${cols.map((col) => `, custom_cols ->> '${col.name}' AS ${col.alias}`).join(", ")}
+      FROM facility
+    `)
+
+    return result.rows
+    
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+const create = async (body) => {
+  console.log("facility data access layer");
+  try {
+    const cols = Object.keys(body)
+    const query = `INSERT INTO facility 
+      (${cols.map((col) => `${col}`).join(", ")}) 
+      VALUES (${cols.map((_, i) => `$${i + 1}`).join(", ")})
+    `
+    await db.query(query, Object.values(body))
+
+    return { message: "Category is succesfully created." }
+
+  } catch (err) {
+    throw new Error(err)
+  }
 }
 
 
 module.exports = {
   getAll,
+  create
 }
