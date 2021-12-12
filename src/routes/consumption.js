8e321d51ca13751fd1b4a70d865ebda025e7a0b4
ozanulutas <oznulutas@gmail.com>
2021-12-12 19:31:58 +0300

    refactor: comment unused code out

--- src/routes/consumption.js
+++ src/routes/consumption.js
@@ -6,11 +6,9 @@ const auth = require("../middlewares/auth");
 
 (async () => {
   router.get("/", consumption.getAll);
-  router.post("/", auth("user"), consumption.create);
-  // router.post("/", auth("user"), await validations(), consumption.create);
-  router.get("/:id", consumption.getById);
-  router.put("/:id", consumption.update);
-  router.delete("/:id", consumption.remove);
+  // router.post("/", auth("user"), consumption.create);
+  // router.put("/:id", consumption.update);
+  // router.delete("/:id", consumption.remove);
 })()
 
 
