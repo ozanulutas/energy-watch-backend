797a3c42c50ac1861ce10fd49f84988081f6f387
ozanulutas <oznulutas@gmail.com>
2021-12-15 00:10:30 +0300

    feat: sd

--- src/routes/facility.js
+++ src/routes/facility.js
@@ -4,13 +4,11 @@ const { facility } = require("../controllers");
 const validation = require("../middlewares/validations/facility");
 const auth = require("../middlewares/auth");
 
-const cors = require("../utils/helpers/allow-cors.js");
-
 
 (async () => {
   const validations = await validation()
 
-  router.get("/", cors, auth("user"), facility.getAll);
+  router.get("/", auth("user"), facility.getAll);
   router.post("/", auth("editor"), validations, facility.create);
   // router.get("/:id", auth("user"), facility.getById);
   router.put("/:id", auth("editor"), validations, facility.update);
