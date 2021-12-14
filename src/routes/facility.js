df342ed5b307dd9d60f47e64e5832da1cdf76d57
ozanulutas <oznulutas@gmail.com>
2021-12-14 23:58:31 +0300

    feat: add allowCors function

--- src/routes/facility.js
+++ src/routes/facility.js
@@ -4,11 +4,13 @@ const { facility } = require("../controllers");
 const validation = require("../middlewares/validations/facility");
 const auth = require("../middlewares/auth");
 
+const cors = require("../utils/helpers/allow-cors.js");
+
 
 (async () => {
   const validations = await validation()
 
-  router.get("/", auth("user"), facility.getAll);
+  router.get("/", cors, auth("user"), facility.getAll);
   router.post("/", auth("editor"), validations, facility.create);
   // router.get("/:id", auth("user"), facility.getById);
   router.put("/:id", auth("editor"), validations, facility.update);
