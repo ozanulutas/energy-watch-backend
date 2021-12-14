d0b5eca123b1414a40f4ab7848917a0700a25034
ozanulutas <oznulutas@gmail.com>
2021-12-14 23:20:57 +0300

    refactor: change postgres conn str

--- src/db/pg-client.js
+++ src/db/pg-client.js
@@ -1,12 +1,15 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  host: "localhost",
-  post: 5432,
-  user: "postgres",
-  password: "123123",
-  database: "energy_watch"
-});
+const { POSTGRES } = process.env
+
+const client = new Client(POSTGRES);
+// const client = new Client({
+//   host: "localhost",
+//   post: 5432,
+//   user: "postgres",
+//   password: "123123",
+//   database: "energy_watch"
+// });
 
 // Client methods are wrapped around a function so logging
 // would be implemented in the future
