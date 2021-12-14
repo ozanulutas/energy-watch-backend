c111992572f393bc0a3e33b2c05452d69605f353
ozanulutas <oznulutas@gmail.com>
2021-12-15 02:25:11 +0300

    feat: add nullable to custom col validaitons

--- src/middlewares/validations/validation-handlers.js
+++ src/middlewares/validations/validation-handlers.js
@@ -2,9 +2,9 @@ const { body } = require('express-validator');
 
 // Map of data type and it's validation handlers with error messages
 module.exports = {
-  integer: (name, type) => body(name).isInt().optional().escape().withMessage(`${name} must be ${type}`),
-  boolean: (name, type) => body(name).isBoolean().optional().escape().withMessage(`${name} must be ${type}`),
-  date: (name, type) => body(name).isDate().optional().escape().withMessage(`${name} must be ${type}`),
-  text: (name, type) => body(name).isAlphanumeric().optional().escape().withMessage(`${name} must be ${type}`),
-  double: (name, type) => body(name).isNumeric().optional().escape().withMessage(`${name} must be ${type}`),
+  integer: (name, type) => body(name).isInt().optional({ nullable: true }).escape().withMessage(`${name} must be ${type}`),
+  boolean: (name, type) => body(name).isBoolean().optional({ nullable: true }).escape().withMessage(`${name} must be ${type}`),
+  date: (name, type) => body(name).isDate().optional({ nullable: true }).escape().withMessage(`${name} must be ${type}`),
+  text: (name, type) => body(name).isAlphanumeric().optional({ nullable: true }).escape().withMessage(`${name} must be ${type}`),
+  double: (name, type) => body(name).isNumeric().optional({ nullable: true }).escape().withMessage(`${name} must be ${type}`),
 };
