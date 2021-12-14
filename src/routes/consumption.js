99e312e0fcd55f9f4d7ac9d07d479c1cac0129c3
ozanulutas <oznulutas@gmail.com>
2021-12-14 12:14:29 +0300

    perf: move validation func from routers to a var

--- src/routes/consumption.js
+++ src/routes/consumption.js
@@ -6,9 +6,11 @@ const auth = require("../middlewares/auth");
 
 
 (async () => {
+  const validations = await validation()
+  
   router.get("/", auth("user"), consumption.getAll);
-  router.post("/", auth("editor"), await validation(), consumption.create);
-  router.put("/:id", auth("editor"), await validation(), consumption.update);
+  router.post("/", auth("editor"), validations, consumption.create);
+  router.put("/:id", auth("editor"), validations, consumption.update);
   router.delete("/:id", auth("admin"), consumption.remove);
 })()
 
--- src/routes/facility.js
+++ src/routes/facility.js
@@ -6,10 +6,12 @@ const auth = require("../middlewares/auth");
 
 
 (async () => {
+  const validations = await validation()
+
   router.get("/", auth("user"), facility.getAll);
-  router.post("/", auth("editor"), await validation(), facility.create);
+  router.post("/", auth("editor"), validations, facility.create);
   // router.get("/:id", auth("user"), facility.getById);
-  router.put("/:id", auth("editor"), await validation(), facility.update);
+  router.put("/:id", auth("editor"), validations, facility.update);
   router.delete("/:id", auth("admin"), facility.remove);
 })()
 
