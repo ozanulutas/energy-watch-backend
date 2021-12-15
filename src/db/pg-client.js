14d99e709cb8d287e620a4e76aa0323fd197086f
ozanulutas <oznulutas@gmail.com>
2021-12-15 18:55:55 +0300

    refactor: change api url

--- src/db/pg-client.js
+++ src/db/pg-client.js
@@ -2,14 +2,14 @@ const { Client } = require("pg");
 
 const { POSTGRES } = process.env
 
-// const client = new Client(POSTGRES);
-const client = new Client({
-  host: "localhost",
-  post: 5432,
-  user: "postgres",
-  password: "123123",
-  database: "energy_watch"
-});
+const client = new Client(POSTGRES);
+// const client = new Client({
+//   host: "localhost",
+//   post: 5432,
+//   user: "postgres",
+//   password: "123123",
+//   database: "energy_watch"
+// });
 
 // Client methods are wrapped around a function so logging
 // would be implemented in the future
